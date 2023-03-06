3784e835eed452be647cd8e619d588fa222cb1fb
elduverx <duverwild@gmail.com>
2023-03-06 15:56:02 +0100

    added channelView, constraints, ibOutlet, dynamic info applied.

--- youtubeClon/Home/View/CustomCells/ChannelCell.swift
+++ youtubeClon/Home/View/CustomCells/ChannelCell.swift
@@ -9,15 +9,27 @@ import UIKit
 
 class ChannelCell: UITableViewCell {
 
-    override func awakeFromNib() {
+  
+  @IBOutlet weak var bannerChannel: UIImageView!
+  
+  @IBOutlet weak var bellImage: UIImageView!
+  @IBOutlet weak var infoLabel: UILabel!
+  @IBOutlet weak var subscriptorsLabel: UILabel!
+  @IBOutlet weak var subscribedLabel: UILabel!
+  @IBOutlet weak var channelName: UILabel!
+  @IBOutlet weak var channelProfileImg: UIImageView!
+  override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+    
+    bellImage.image = UIImage(named: "bell")?.withRenderingMode(.alwaysTemplate)
+    bellImage.tintColor = UIColor(named: "grayColor")
+    channelProfileImg.layer.cornerRadius = 51/2
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
+  func configCell(model:ChannelModel.Items){
+    channelName.text = model.snippet.title
+    infoLabel.text = model.snippet.description
+    subscriptorsLabel.text = "\(model.statistics?.subscriberCount ?? "0") subscribers · \(model.statistics?.videoCount ?? "0") videos"
+  }
     
 }
--- youtubeClon/Home/View/HomeViewController.swift
+++ youtubeClon/Home/View/HomeViewController.swift
@@ -55,6 +55,7 @@ extension HomeViewController : UITableViewDelegate,UITableViewDataSource{
       guard let channelCell = tableView.dequeueReusableCell(withIdentifier: "\(ChannelCell.self)", for: indexPath) as? ChannelCell else{
         return UITableViewCell()
       }
+      channelCell.configCell(model: channel[indexPath.row])
       return channelCell
     }else   if let playlistItems = item as? [PlaylistItemsModel.Item]{
       guard let playlistItemsCell = tableView.dequeueReusableCell(withIdentifier: "\(VideoCell.self)", for: indexPath) as? VideoCell else{
